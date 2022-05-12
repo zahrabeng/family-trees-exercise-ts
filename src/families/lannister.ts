55018bd1b3ce15aecb4555e7f8049382b0ca9fc0
nbogie <neillbogie@googlemail.com>
2022-05-12 03:46:11 +0100

    stubs for lannister and stark

--- src/families/lannister.ts
+++ src/families/lannister.ts
@@ -2,28 +2,5 @@ import { Person } from "../personTypes";
 
 export function createLannisterTree(): Person {
     //https://en.wikipedia.org/wiki/Template:Family_tree_of_Tytos_Lannister
-    const cerseisKids: Person[] = [
-        { name: "Joffrey", children: [] },
-        { name: "Myrcella", children: [] },
-        { name: "Tommen", children: [] },
-    ]
-
-    const tywin: Person = {
-        name: "Tywin", children: [
-            { name: "Cersei", children: cerseisKids },
-            { name: "Ser Jaime", children: [] },
-            { name: "Tyrion", children: [] },
-        ]
-    };
-
-    const topPerson: Person = {
-        name: "Tytos", children: [tywin,
-            { name: "Genna", children: [] },
-            { name: "Kevan", children: [] },
-            { name: "Tygett", children: [] },
-            { name: "Gerion", children: [] },
-        ]
-    };
-
-    return topPerson;
+    return { name: "LANNISTER: YOU NEED TO IMPLEMENT THIS", children: [] };
 }
